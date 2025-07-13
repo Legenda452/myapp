35fe94d686d6c2734a8ea7ac1aae2a4ff9ed3c8b
Andrew <krutitaly@gmail.com>
2025-07-14 01:14:00 +0300

    Update app.py and requrements.txt

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 
-BOT_USERNAME = 'Squzeey'
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    if 'tgWebAppPlatform' in request.args:
-        return render_template('index.html', bot_username=BOT_USERNAME)
-    else:
-        return "Это должно быть открыто в Telegram Mini App."
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False) # Отключите debug в production
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
